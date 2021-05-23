feb1f00a50bf2d106ac5959daf6c95fc109bf59b
kyme254 <kimanzitech254@gmail.com>
2021-05-23 21:04:15 +0300

    Initial commit

---
+++ app/src/main/java/com/example/wasavu/fragments/CallsTabFragment.java
@@ -0,0 +1,23 @@
+package com.example.wasavu.fragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.wasavu.R;
+
+public class CallsTabFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_calls_tab, container, false);
+
+        return rootView;
+    }
+}
